7dd42d546fff65f569e5a82e81037d443d3eebac
Rian Ari Sasono <rianono48@gmail.com>
2022-10-23 17:22:54 +0700

    Typo middleware/auth

--- app/transaction/router.js
+++ app/transaction/router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { index, update } = require('./controller');
 
-const { verifyUser } = require('../../middleware/auth');
+const { verifyUser } = require('../../middleware/Auth');
 
 router.use(verifyUser);
 /* GET home page. */
